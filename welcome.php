5f81260aa53df5538b256258900d0630ca5d397f
AbhithaVish <162031145+AbhithaVish@users.noreply.github.com>
2024-07-17 20:58:21 +0530

    nav bar final

--- welcome.php
+++ welcome.php
@@ -1,71 +1,81 @@
 <?php
-// Start the session 
 session_start();
-
-// Include the database connection
-include_once('connection.php');
-
-// Close the database connection
-$conn->close();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header class="header">
-        <nav class="nav container">
-            <div class="nav__data">
-                <a href="index.html" class="nav__logo">
-                    <i class="ri-planet-line"></i> The Gallery Cafe
-                </a>
-            </div>
-            <div class="nav__menu" id="nav-menu">
-                <ul class="nav__list">
-                    <li><a href="index.php" class="nav__link">Home</a></li>
-                    <li><a href="index.php" class="nav__link">Menu</a></li>
-                    <li><a href="index.php" class="nav__link">Live</a></li>
-                    <li><a href="index.php" class="nav__link">Order</a></li>
-                    <li><a href="index.php" class="nav__link">Reservation</a></li>
-                    <li><a href="about.php" class="nav__link">About Us</a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    
-    <div class="main-banner">
-        <div class="main-banner__content">
-            <h1>Our Newly</h1>
-            <h1>Introduced</h1>
-            <h1>Item</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nobis cumque aspernatur dolore tempora earum nihil quas expedita, magni, deserunt, molestias doloremque. Illo odio maiores vel vitae laudantium. Pitiis est hic, inventore ab officia numquam perspiciatis?</p>
-            <button class="order">Order Now</button>
-        </div>
-        <img src="Images/steak.png" alt="image">
-    </div>
+  <title>Login</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <div class="mini-boxes">
-        <div class="box">
-            <h2>Reservation</h2>
-            <button>Book Now</button>
+  <!-- bootstrap for make responsive -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+<?php
+    if (isset($_SESSION['error'])) {
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">'
+            . $_SESSION['error'] .
+            '<button type="button" id="search-icon" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+             </button>
+        </div>';
+        unset($_SESSION['error']);
+    }
+?>
+
+
+  <section class="vh-100">
+    <div class="container py-5 h-100">
+      <div class="row d-flex align-items-center justify-content-center h-100">
+        <h1 class="text-center h1 fw-bold mb-4 mx-1 mx-md-3 mt-3">The Gallery Cafe</h1>
+        <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+          <form action="login.php" method="post">
+            
+            <!-- <p class="text-center h1 fw-bold mb-4 mx-1 mx-md-3 mt-3">Login </p> -->
+
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+              <label class="form-label" for="form1Example13"> <i class="bi bi-person-circle"></i> Username</label>
+              <input type="text" id="form1Example13" class="form-control form-control-lg py-3" name="username" autocomplete="off" placeholder="enter your username" style="border-radius:25px ;" />
+
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+              <label class="form-label" for="form1Example23"><i class="bi bi-chat-left-dots-fill"></i> Password</label>
+              <input type="password" id="form1Example23" class="form-control form-control-lg py-3" name="password" autocomplete="off" placeholder="enter your password" style="border-radius:25px ;" />
+
+            </div>
+
+
+            <!-- Signin button -->
+            <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+              <input type="submit" value="Sign in" name="login" class="btn btn-warning btn-lg text-light my-2 py-3" style="width:100% ; border-radius: 30px; font-weight:600; background-color: #3333ff; border-color: #3333ff; color: #FFFFFF;" />
+          
+            </div>
+          </form>
         </div>
-        <div class="box">
-            <h2>Order Now</h2>
-            <button>Order</button>
-        </div>
-        <div class="box">
-            <h2>Tables</h2>
-            <button>View Tables</button>
-        </div>
-        <div class="box">
-            <h2>Menu</h2>
-            <button>See Menu</button>
-        </div>
+      </div>
+      
     </div>
+  </section>
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+
+  <!-- Include Bootstrap JS and dependencies (optional) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
